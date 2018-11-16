e22c04ac63f0f54d3630746c633df1059377fd6f
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-16 10:00:30 -0500

    ticket-request.rb

---
+++ ch2/2.3/ticket-request.rb
@@ -0,0 +1,29 @@
+ticket = Object.new
+
+def ticket.date
+  "1903-01-02"
+end
+def ticket.venue
+  "Town Hall"
+end
+def ticket.event
+  "Author's reading"
+end
+def ticket.performer
+  "Mark Twain"
+end
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+def ticket.price                    #1
+  5.50
+end
+
+print "Information desired: "
+request = gets.chomp
+
+if ticket.respond_to?(request)
+  puts ticket.send(request)
+else
+  puts "No such information is available."
+end
