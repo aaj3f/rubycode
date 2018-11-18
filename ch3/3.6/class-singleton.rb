98af77ffc74b42bb5b29e8a769c5c80b314663de
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-18 14:22:13 -0500

    added 3.5 and 3.6 directories

---
+++ ch3/3.6/class-singleton.rb
@@ -0,0 +1,22 @@
+class Ticket
+  attr_reader :venue, :date
+  attr_accessor :price
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
+end
+
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new("Town Hall", "2013-11-12")
+cc = Ticket.new("Convention Center", "2014-12-13")
+fg = Ticket.new("Fairgrounds", "2015-10-11")
+
+th.price = 12.55
+cc.price = 10.00
+fg.price = 18.00
+
+puts "The highest-priced ticket is the one for #{Ticket.most_expensive(th, cc, fg).venue}."
