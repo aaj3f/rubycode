f397f535e3976448c59c69a4c3cb6569abc28999
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-21 10:28:58 -0500

    cargohold.rb

--- ch4/4.4/cargohold.rb
+++ ch4/4.4/cargohold.rb
@@ -1,6 +1,30 @@
+require "pry"
+
 class Stack
   attr_reader :stack
   def initialize
     @stack = []
   end
+  def add_to_stack(obj)
+    @stack << obj
+  end
+  def take_from_stack
+    @stack.pop
+  end
 end
+
+class Suitcase
+end
+
+class CargoHold < Stack
+  def load_and_report(obj)
+    print "Loading object "
+    puts obj.object_id
+    add_to_stack(obj)
+  end
+  def unload
+    take_from_stack
+  end
+end
+
+binding.pry
