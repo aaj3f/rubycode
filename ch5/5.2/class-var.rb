2d48abab1e32456a9d2b9534939a37ed77b0af9b
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-22 12:22:58 -0500

    edited class-var.rb to remove all class variables

--- ch5/5.2/class-var.rb
+++ ch5/5.2/class-var.rb
@@ -1,35 +1,61 @@
 require "pry"
 
 class Car
-  @@makes = []
-  @@cars = {}
-  @@total_count = 0
+  # @@makes = []
+  # @@cars = {}
+  # @@total_count = 0
 
   attr_reader :make
 
   def self.total_count
-    @@total_count
+    @total_count ||= 0
+  end
+
+  def self.total_count=(n)
+    @total_count = n
+  end
+
+  def self.makes
+    @makes ||= []
+  end
+
+  def self.makes=(n)
+    unless @makes
+      @makes = []
+    end
+    @makes << n
+  end
+
+  def self.cars
+    @cars ||= {}
+  end
+
+  def self.cars=(n)
+    unless @cars
+      @cars = {}
+    end
+    @cars[n] = 0
   end
 
   def self.add_make(make)
-    unless @@makes.include?(make)
-      @@makes << make
-      @@cars[make] = 0
+    unless makes.include?(make)
+      self.makes = make
+      self.cars = make
     end
   end
 
   def initialize(make)
-    if @@makes.include?(make)
+    if self.class.makes.include?(make)
       puts "Creating a new #{make}!"
       @make = make
-      @@cars[make] += 1
-      @@total_count += 1
+      self.class.cars[make] += 1
+      self.class.total_count += 1
     else raise "No such make: #{make}."
     end
   end
 
   def make_mates
-    @@cars[self.make]
+    self.class.cars[self.make]
   end
 end
 
