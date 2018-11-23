9709dff086ec8f321c2a41d6982d93537862a7c2
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-23 18:18:58 -0500

    my-map.rb

--- ch6/6.3/my-map.rb
+++ ch6/6.3/my-map.rb
@@ -1,13 +1,19 @@
 require "pry"
 
 class Array
-  def my_map
-    c = 0
-    acc = []
-    until c == size
-      acc << yield(self[c])
-      c += 1
+
+  def my_each
+    counter = 0
+    until counter == size
+      yield(self[counter])
+      counter += 1
     end
+    self
+  end
+
+  def my_map
+    acc = []
+    my_each { |x| acc << yield(x) }
     acc
   end
 end
