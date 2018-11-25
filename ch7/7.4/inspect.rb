53dcc4d34c21e564b34de0e331973f36a25aab64
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-24 21:17:06 -0500

    7.4

---
+++ ch7/7.4/inspect.rb
@@ -0,0 +1,14 @@
+require "pry"
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def inspect
+    @name
+  end
+end
+
+david = Person.new("David")
+
+binding.pry
---
+++ ch7/7.4/splat.rb
@@ -0,0 +1,9 @@
+require "pry"
+
+def combine_names(first_name, last_name)
+  first_name + " " + last_name
+end
+
+names = ["Andrew", "Johnson"]
+
+binding.pry
